29fd155e61ef58429d8eb6052fbf8dbc9c221e11
Anton Dergunov <1549134+anton-dergunov@users.noreply.github.com>
2021-05-09 22:10:57 +0100

    Initial config from MacOS version

---
+++ emacs/init.el
@@ -0,0 +1,63 @@
+;; Disable the splash screen
+(setq inhibit-splash-screen t)
+
+(tool-bar-mode -1)
+(set-face-attribute 'default nil :font "Monaco-14")
+
+(setq default-directory "/Users/anton/mywork/Dropbox/notes/Plans/Org/")
+
+(define-key global-map "\C-ca" 'org-agenda)
+
+(setq org-todo-keywords '((sequence "TODO(t)" "INPR(i)" "WAIT(w)" "SMDY(s)" "|" "DONE(d)")))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/mywork/Dropbox/notes/Professional/Org_mode.org"))
+ '(package-selected-packages '(solarized-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background nil)
+
+;; Don't change the font for some headings and titles
+(setq solarized-use-variable-pitch nil)
+
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line nil)
+
+;; Use less bolding
+(setq solarized-use-less-bold t)
+
+;; Use more italics
+(setq solarized-use-more-italic t)
+
+;; Use less colors for indicators such as git:gutter, flycheck and similar
+(setq solarized-emphasize-indicators nil)
+
+;; Don't change size of org-mode headlines (but keep other size-changes)
+(setq solarized-scale-org-headlines nil)
+
+;; Avoid all font-size changes
+(setq solarized-height-minus-1 1.0)
+(setq solarized-height-plus-1 1.0)
+(setq solarized-height-plus-2 1.0)
+(setq solarized-height-plus-3 1.0)
+(setq solarized-height-plus-4 1.0)
+
+(load-theme 'solarized-light t)
+
